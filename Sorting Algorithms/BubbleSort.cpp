28f049ff19717db309d75f89068c138d779a0f2e
Hemanth Reddy <hreddy281@gmail.com>
2022-03-23 00:42:23 +0530

    Bubble Sort(23/03/22)

--- Sorting Algorithms/BubbleSort.cpp
+++ Sorting Algorithms/BubbleSort.cpp
@@ -1,5 +1,15 @@
+//Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.
+
 //Best Case : O(N)
 //Worst Case : O(N^2)
+
+//Inplace Sorting : Yes
+//Stable : Yes
+
+//Inplace Sorting Algorithm uses constant space for producing the output.
+
+//Links : https://www.geeksforgeeks.org/bubble-sort/
+//https://www.youtube.com/watch?v=zOhUavxlzw4
 
 #include<iostream>
 #include<vector>
@@ -20,7 +30,7 @@ void BubbleSort(vector<int>& v,int n){
     for(i=0;i<n-1;i++){ //As for each ith iteration, ith largest number is getting sorted to its right place. 
                         //As sorting n-1 elements means the remaining 1 element is also sorted, we run n-1 loops.
         swapped = false;//Swapped is to check if the array is sorted in btwn loops so that we can exit early.
-        for(j=i;j<n-i-1;j++){// Here we kept n-i-1 as we know that we are comparing the element to its next element,
+        for(j=0;j<n-i-1;j++){// Here we kept n-i-1 as we know that we are comparing the element to its next element,
                              //to avoid any errors when j is the last element.   
             if(v[j] > v[j+1]){ // -i because we know that the last i elements would be sorted.So we don't include them
                 swap(v[j],v[j+1]);
@@ -46,6 +56,14 @@ int main(){
         cin >> elem;
         v.push_back(elem);
     }
+
+    cout << "Before Sorting: ";
+    PrintVector(v,n);
+
+    BubbleSort(v,n);
+
+    cout << "After Sorting: ";
+    PrintVector(v,n);
 
     return 0;
 }
--- Sorting Algorithms/SelectionSort.cpp
+++ Sorting Algorithms/SelectionSort.cpp
@@ -1,3 +1,14 @@
+//The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order) from unsorted part and putting it at the beginning.
+
+//Best Case : O(N^2)
+//Worst Case : O(N^2)
+
+//Inplace Sort : Yes
+//Stable : No Ex : 5(a) 3 4 5(b) 2 6 8
+
+//Link : https://www.geeksforgeeks.org/selection-sort/
+//https://www.youtube.com/watch?v=UdO2NeHB46c&t=2s
+
 #include <iostream>
 #include <vector>
 
@@ -11,6 +22,18 @@ void printVector(vector<int> arr)
         cout << arr[i] << " ";
     }
     cout << endl;
+}
+
+void SelectionSort(vector<int>& v,int n){
+    int i,j,minIndex;
+    for(i=0;i<n-1;i++){
+        minIndex = i;
+        for(j=i+1;j<n;j++){
+            minIndex = v[j] < v[minIndex] ? j : minIndex;
+        }
+
+        swap(v[minIndex],v[i]);
+    }
 }
 
 int main()
@@ -30,6 +53,14 @@ int main()
     }
 
     cout << arr.size() << endl;
+
+    cout << " Before sorting the array:";
     printVector(arr);
+
+    SelectionSort(arr,n);
+
+    cout << " After sorting the array:";
+    printVector(arr);
+
     return 0;
 }
